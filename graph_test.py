862a59b7738cba6a521fc0e34f3f477fa7cc4606
kasukabe001 <ail@fuji.email.ne.jp>
2026-03-16 11:01:10 +0900

    Update graph_test.py

--- graph_test.py
+++ graph_test.py
@@ -1,27 +1,18 @@
-import plotly.graph_objects as go
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
 
-# 1. データの準備
-x_data = ['January', 'February', 'March', 'April', 'May']
-y_data = [10, 22, 15, 28, 20]
+@st.cache
+def load_data():
+    iris = datasets.load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['target'] = iris.target_names[iris.target]
+    return df
 
-# 2. グラフ（棒グラフ）の作成
-fig = go.Figure(data=[go.Bar(
-    x=x_data,
-    y=y_data,
-    marker_color='indianred' # 棒の色をオシャレに
-)])
+df = load_data()
+targets = list(df.target.unique())
+selected_targets = st.multiselect('select targets', targets, default=targets)
+df = df[df.target.isin(selected_targets)]
 
-# 3. レイアウトの設定（タイトルなど）
-fig.update_layout(
-    title='月別売上データ',
-    xaxis_title='月',
-    yaxis_title='売上（万円）',
-    template='plotly_white' # 白背景の清潔感あるテーマ
-)
+st.dataframe(df)
 
-# 4. HTMLファイルとして保存
-# これをサーバーにアップロードすればWebで公開できます
-# fig.write_html('my_graph.html')
-
-# 5. 確認のために自動でブラウザを開く（任意）
-fig.show()
