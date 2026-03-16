e3b700db0dc7e4033ed601b2a98cc8b1c2770128
kasukabe001 <ail@fuji.email.ne.jp>
2026-03-16 15:44:02 +0900

    Update graph_test.py

--- graph_test.py
+++ graph_test.py
@@ -14,7 +14,7 @@ sales_data = pd.DataFrame({
 # monthly_sales = sales_data.groupby(sales_data['date'].dt.to_period('M'))['sales'].sum().reset_index()
 # monthly_sales['date'] = monthly_sales['date'].dt.to_timestamp()
 category_sales = sales_data.groupby(sales_data['date'].dt.to_period('M'))['sales'].sum().reset_index()
-cstegory_sales['date'] = monthly_sales['date'].dt.to_timestamp()
+cstegory_sales['date'] = cstegory_sales['date'].dt.to_timestamp()
 
 import streamlit as st
 
